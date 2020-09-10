a896bb8db2c25ceacb09f7f1696d18d9054470a0
Maria Lopez <lop17160@uvg.edu.gt>
2020-09-09 19:01:00 -0600

    bulk upload funcionando, falta parametrizarlo

--- Jobs/bulk-contenido.py
+++ Jobs/bulk-contenido.py
@@ -11,23 +11,22 @@ content = pd.read_csv('Jobs\seleccionMultiple.csv')
 content.fillna(value='', inplace=True) 
 counter = 1
 dictcounter = 0
-data = {'id': str(counter)}
+data = {'_id': str(counter)}
 toinsert = []
 for c in range(len(content.columns)):
     data[content.columns[c]] = ''
 
 for x in range(len(content)):
     for key in data:
-        if key == 'id':
+        if key == '_id':
             data[key] = str(counter)
         else:
             t = content.iat[x, dictcounter]
             data[key] = str(t)
             dictcounter += 1
+    seleccion.insert_one(data)
     toinsert.append(data)
     dictcounter = 0
     counter += 1
 
-for x in range(len(toinsert)):
-    x = seleccion.insert_one(toinsert[x])
 
